98da7af8d07e0e464090ddff933adb008555ecd7
Michelle Clement <michellelclement@gmail.com>
2022-06-07 09:32:26 +0000

    Animate each letter of logo

--- app.js
+++ app.js
@@ -25,4 +25,14 @@ tl.fromTo(".cta-btn", { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, "<");
 // Split text
 const logo = document.querySelector(".logo");
 const letters = logo.textContent.split("");
-console.log(letters);
+logo.textContent = "";
+letters.forEach((letter) => {
+  logo.innerHTML += '<span class="letter">' + letter + "</span>";
+});
+
+gsap.set(".letter", { display: "inline-block" });
+gsap.fromTo(
+  ".letter",
+  { y: "100%" },
+  { y: 0, delay: 2, stagger: 0.05, ease: "back.out(3)" }
+);
