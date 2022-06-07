eff8c48da103c3d1dc1ceda6996ef4c359f94b6b
Michelle Clement <michellelclement@gmail.com>
2022-06-07 09:15:13 +0000

    Amend duration typo and cta5 typo

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const tl = gsap.timeline({
-  defaults: { durcation: 0.75, ease: "power3.easeOut" }
+  defaults: { duration: 0.75, ease: "power3.easeOut" }
 });
 
 tl.fromTo(
@@ -17,9 +17,12 @@ tl.fromTo(
 tl.fromTo(".cta1", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
 tl.fromTo(".cta3", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
 tl.fromTo(".cta2", { y: "100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<20%");
-
 tl.fromTo(".cta4", { x: "100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
 tl.fromTo(".cta6", { x: "-100%", opacity: 0.5 }, { x: 0, opacity: 1 }, "<20%");
 tl.fromTo(".cta5", { y: "100%", opacity: 0.5 }, { y: 0, opacity: 1 }, "<20%");
-
 tl.fromTo(".cta-btn", { y: 20, opacity: 0 }, { y: 0, opacity: 1 }, "<");
+
+// Split text
+const logo = document.querySelector(".logo");
+const letters = logo.textContent.split("");
+console.log(letters);
